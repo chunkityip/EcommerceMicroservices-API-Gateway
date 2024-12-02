8699ebe0e7015089d3dfd88eb7cd557facc26977
chunkityip <alex0917lfo@hotmail.com>
2024-12-02 13:27:01 -0500

    feat: implement all service into one Swagger docu

--- src/main/java/com/example/api_gateway/routes/Routes.java
+++ src/main/java/com/example/api_gateway/routes/Routes.java
@@ -1,5 +1,6 @@
 package com.example.api_gateway.routes;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,10 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
 
@@ -17,6 +22,16 @@ public class Routes {
     public RouterFunction<ServerResponse> productServiceRoute() {
         return GatewayRouterFunctions.route("product_server")
                 .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://locahost:8080"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http(productServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceSwaggerCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 
